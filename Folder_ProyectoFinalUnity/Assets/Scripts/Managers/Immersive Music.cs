f0da5f3f27da3cdb3eaddf53e988d962e03d770f
Adrian AB <jose.anchiraico@tecsup.edu.pe>
2024-11-28 01:15:58 -0500

    Diagram Class Update

---
+++ Folder_ProyectoFinalUnity/Assets/Scripts/Managers/Immersive Music.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmersiveMusic : MonoBehaviour
+{
+    [Header("Audio Settings")]
+    [SerializeField] private AudioSource bonfireAudio; 
+    [SerializeField] private float maxVolume;      
+    [SerializeField] private float fadeSpeed;
+    [SerializeField] private Transform playerTransform;                   
+    private SphereCollider audioRangeCollider;
+
+    private void Start()
+    {
+        audioRangeCollider = GetComponent<SphereCollider>();
+        bonfireAudio.volume = 0.1f; 
+    }
+    private void Update()
+    {
+        if (playerTransform != null)
+        {
+            float distance = Vector3.Distance(playerTransform.position, transform.position);
+
+            float normalizedVolume = Mathf.Clamp01(1 - (distance / audioRangeCollider.radius));
+
+            bonfireAudio.volume = Mathf.Lerp(bonfireAudio.volume, normalizedVolume * maxVolume, Time.deltaTime * fadeSpeed);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!bonfireAudio.isPlaying)
+            {
+                bonfireAudio.Play();
+            }
+        }
+    }
+}
